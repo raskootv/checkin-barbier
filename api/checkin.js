a3b8e80e5cfa984bb62237f4cd8cf7fbb893cf91
raskootv <raskootv@gmail.com>
2025-07-22 23:05:25 +0200

    yes

--- api/checkin.js
+++ api/checkin.js
@@ -1,6 +1,7 @@
 // checkin.js
-const SUPABASE_URL = "https://odrgqvseddbbvvywguml.supabase.co";
+const SUPABASE_URL = "https://odrgqvseddbbvvywguml.supabase.co/functions/v1/checkin";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kcmdxdnNlZGRiYnZ2eXdndW1sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyMTI4MzQsImV4cCI6MjA2ODc4ODgzNH0.zbWnDGbsdhY6u_YUs_2cLmN3UxtLp9oPn8l89iLWUyE";
+const FUNCTION_URL = "https://odrgqvseddbbvvywguml.supabase.co/functions/v1/checkin";
 
 async function checkIn() {
   const numero = document.getElementById('numero').value.trim();
